f5150eeacb15ce30ecda10c337174ed70cc75857
advy99 <advy99@gmail.com>
2020-10-31 21:46:02 +0100

    estructura del fichero financiacion

---
+++ ejercicio_funciones_redondeo/financiacion.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+
+def redondear ( numero, decimales=2 ):
+    """
+    Funcion que recibe dos parámetros, un número y la cantidad de decimales
+    a redondear, por defecto dos. Como salida da el numero redondeado a tantos
+    decimales como se indiquen.
+    """
+
+    #TODO
+    redondeado = numero
+
+
+    return redondeado
+
+def calcular_capital_final ( capital_inicial, interes ):
+    """
+    Función que dada una cantidad de capital (float con dos decimales), y un
+    interes en tanto por ciento (con dos decimales), da la suma de capital
+    inicial con el interes obtenido, redondeado a dos decimales
+    """
+
+
+
+if __name__ == "main":
+    print("Módulo de financiacion llamado como fichero principal")
+
