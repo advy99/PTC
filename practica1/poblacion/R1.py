98ad845012594bae848d3d0d1fb0e0f43730869c
advy99 <advy99@gmail.com>
2020-11-19 00:03:25 +0100

    comenzamos práctica 1

---
+++ practica1/poblacion/R1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import funciones_csv
+
+dicc = funciones_csv.leer_csv_a_diccionario("entradas/poblacionProvinciasHM2010-17.csv", ";", "Total Nacional", "Notas", "Provincia;T2017;T2016;T2015;T2014;T2013;T2012;T2011;T2010;H2017;H2016;H2015;H2014;H2013;H2012;H2011;H2010;M2017;M2016;M2015;M2014;M2013;M2012;M2011;M2010;");
+
