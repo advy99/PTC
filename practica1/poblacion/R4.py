c56a7bc94bd9bd61700e69f2794a60014a26293d
advy99 <advy99@gmail.com>
2020-11-22 13:05:01 +0100

    R4: separado calculo de salida de datos

--- practica1/poblacion/R4.py
+++ practica1/poblacion/R4.py
@@ -5,8 +5,9 @@ import R3
 import numpy as np
 import funciones_html
 
-def main():
-    dicc_pob_com, _ = R3.main()
+
+def var_pob_comunidades():
+    dicc_pob_com, _ = R3.com_mas_pobladas()
 
     dicc_var_com = {}
 
@@ -49,6 +50,13 @@ def main():
             dicc_var_com[clave][contador_array] = (dicc_pob_com[clave][i] / dicc_pob_com[clave][i+1]) * 100
             contador_array += 1
 
+    return dicc_var_com
+
+
+
+def main():
+
+    dicc_var_com = var_pob_comunidades()
 
     cabecera = ["Comunidad", "Abs. T2017", "Abs. T2016", "Abs. T2015","Abs. T2014","Abs. T2013","Abs. T2012","Abs. T2011","Abs. H2017","Abs. H2016","Abs. H2015","Abs. H2014","Abs. H2013","Abs. H2012","Abs. H2011","Abs. M2017","Abs. M2016","Abs. M2015","Abs. M2014","Abs. M2013","Abs. M2012","Abs. M2011","Rel. T2017", "Rel. T2016", "Rel. T2015","Rel. T2014","Rel. T2013","Rel. T2012","Rel. T2011","Rel. H2017","Rel. H2016","Rel. H2015","Rel. H2014","Rel. H2013","Rel. H2012","Rel. H2011","Rel. M2017","Rel. M2016","Rel. M2015","Rel. M2014","Rel. M2013","Rel. M2012","Rel. M2011"]
 
@@ -56,7 +64,6 @@ def main():
 
     funciones_html.diccionario_a_tabla_html(ruta_resultado, dicc_var_com, "Variaciones por comunidad", "Resultados R4: Variaciones por comunidad", cabecera, css = "estilo.css")
 
-    return dicc_var_com
 
 if __name__ == "__main__":
     main()
