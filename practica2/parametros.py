45517d1957bc5700b274f93e3a6692812c281abf
advy99 <advy99@gmail.com>
2021-01-04 19:00:52 +0100

    tenemos los parámetros en una clase con todos los atributos estaticos
    son estaticos porque los usaremos en todos los scrips, los mismo parámetros, no tendremos dos instancias distintas de parámetros

---
+++ practica2/parametros.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Parametros:
+    iteraciones = 50
+    cerca = 0.5
+    media = 1.5
+    lejos = 2.5
+    min_puntos = 0
+    max_puntos = 0
+    umbral_distancia = 0
+
+
+
