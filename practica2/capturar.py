bf5cae21d3a105f334db143dd50d23082f0fc182
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-01-19 00:06:35 +0100

    seguimos con el capturar, aunque no captura cosas

--- practica2/capturar.py
+++ practica2/capturar.py
@@ -5,52 +5,76 @@
 from parametros import Parametros
 import vrep
 import json
+import time
+import glob
+import re
 
-
-
-def main(fichero, id_cliente):
+def capturarPositivos(fichero, id_cliente):
     
     # sensor de la camara
-    _, camhandle = vrep.simxGetObjectHandle(clientID, 'Vision_sensor', vrep.simx_opmode_oneshot_wait)
+    _, camhandle = vrep.simxGetObjectHandle(id_cliente, 'Vision_sensor', vrep.simx_opmode_oneshot_wait)
     
     # referencia a la persona, para moverla
-    _, personhandle = vrep.simxGetObjectHandle(clientID, 'Bill', vrep.simx_opmode_oneshot_wait)
+    _, personhandle = vrep.simxGetObjectHandle(id_cliente, 'Bill', vrep.simx_opmode_oneshot_wait)
 
-
+    # esperamos 0.5 para el ciclo de lectura, como nos pide el guión
+    segundos = 0.5
     
     #Iniciar la camara y esperar un segundo para llenar el buffer
     _, resolution, image = vrep.simxGetVisionSensorImage(id_cliente, camhandle, 0, vrep.simx_opmode_streaming)
-    time.sleep(1)
+    time.sleep(segundos)
+        
+    cabecera = {"TiempoSleep":segundos,
+                "MaxIteraciones":Parametros.iteraciones}
     
-    segundos = 0.5
-    
-    cabecera={"TiempoSleep":segundos,
-                "MaxIteraciones": Parametros.iteraciones}
-    
-    fichero_laser=open(fichero, "w")
+    fichero_laser = open(fichero, "w")
 
     fichero_laser.write(json.dumps(cabecera)+'\n')
 
     
+
+    distancia = 0.0
+    a_recorrer = 0.0
+    
+    if re.findall("Cerca", fichero):
+        vrep.simxSetObjectPosition(id_cliente, personhandle, -1,[Parametros.cerca, 0, 0.0], vrep.simx_opmode_oneshot)
+        a_recorrer = Parametros.media - Parametros().cerca
+        distancia = Parametros.cerca + a_recorrer 
+    elif re.findall("Media", fichero):
+        vrep.simxSetObjectPosition(id_cliente, personhandle, -1,[Parametros.media, 0, 0.0], vrep.simx_opmode_oneshot)
+        a_recorrer = Parametros.lejos - Parametros().media
+        distancia = Parametros.media + a_recorrer
+    elif re.findall("Lejos", fichero):
+        vrep.simxSetObjectPosition(id_cliente,personhandle, -1,[Parametros.lejos, 0, 0.0], vrep.simx_opmode_oneshot)
+        distancia = Parametros.lejos + 1
     
     for i in range(1, Parametros.iteraciones):
-        puntosx=[] #listas para recibir las coordenadas x, y z de los puntos detectados por el laser
-        puntosy=[]
-        puntosz=[]
+        #listas para recibir las coordenadas x, y z de los puntos detectados por el laser
+        puntosx = []
+        puntosy = []
+        puntosz = []
         
-        returnCode, signalValue = vrep.simxGetStringSignal(id_cliente,'LaserData',vrep.simx_opmode_buffer) 
+        returnCode, signalValue = vrep.simxGetStringSignal(id_cliente,'Vision_sensor',vrep.simx_opmode_buffer)
         
-        # esperamos 0.5 para el ciclo de lectura, como nos pide el guión
-        time.sleep(segundos) #esperamos un tiempo para que el ciclo de lectura de datos no sea muy rápido
+        #esperamos un tiempo para que el ciclo de lectura de datos no sea muy rápido
+        time.sleep(segundos) 
         
-        datosLaser=vrep.simxUnpackFloats(signalValue)
+        datosLaser = vrep.simxUnpackFloats(signalValue)
         
         for indice in range(0,len(datosLaser),3):
             puntosx.append(datosLaser[indice+1])
             puntosy.append(datosLaser[indice+2])
             puntosz.append(datosLaser[indice])
     
-        lectura={"Iteracion":i, "PuntosX":puntosx, "PuntosY":puntosy}
-        fichero_laser.write(json.dumps(lectura)+'\n')
-
+        # escribimos los datos
+        lectura = {"Iteracion":i, "PuntosX":puntosx, "PuntosY":puntosy}
+        fichero_laser.write(json.dumps(lectura) + '\n')
+        
+        # movemos a la persona
+        returnCode = vrep.simxSetObjectPosition(id_cliente, personhandle, -1, [distancia - a_recorrer * i / Parametros.iteraciones, 0.0, 0.0], vrep.simx_opmode_oneshot)
+                                                                               
+                                   
+    fichero_laser.close()
+                                                                               
+                                                                               
    
