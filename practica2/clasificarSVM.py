bd569455aab86671430d3d09dab447ad1e293f60
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-01-20 22:56:39 +0100

    acabado clasificador

--- practica2/clasificarSVM.py
+++ practica2/clasificarSVM.py
@@ -22,9 +22,26 @@ simplefilter(action='ignore', category=DeprecationWarning)
 def comparar_reales(a, b, epsilon = 0.0005):
     return abs(a - b) < epsilon
 
-def probarSVM(datos_x, datos_y, x_train, y_train, x_test, y_test, svc, parametros):
+def entrenar_modelo(datos_x, datos_y, x_train, y_train, x_test, y_test, svc):
+    svc.fit(x_train, y_train)
+    
+    y_pred = svc.predict(x_test)
+
+    print("Matriz de confusión Filas: verdad Columnas: predicción")
+    print(confusion_matrix(y_test, y_pred))
+    
+    
+    # predicciones con validación cruzada:
+    scores = cross_val_score(svc, x_test, y_test, cv=5, n_jobs=-1)
+
+    # exactitud media con intervalo de confianza del 95%
+    print("Accuracy 5-cross validation: %0.4f (+/- %0.4f)" % (scores.mean(), scores.std() * 2))
+    
+    return scores, svc
+
+def entrenar_grid(datos_x, datos_y, x_train, y_train, x_test, y_test, svc, parametros):
         
-    grid_search = GridSearchCV(svc, parametros)
+    grid_search = GridSearchCV(svc, parametros, n_jobs=-1)
     
     grid_search.fit(x_train, y_train)
 
@@ -55,9 +72,6 @@ def clasificar_piernas():
     datos_x = datos_piernas.drop("clase", axis = 1)
     datos_y = datos_piernas["clase"]
     
-    # normalizamos los datos, si no tarda mucho
-    datos_x = (datos_x - datos_x.mean()) / (datos_x.max() - datos_x.min())
-
     
     # dividimos train y test, 20% de test
     x_train, x_test, y_train, y_test = train_test_split(datos_x, datos_y, test_size = 0.20, random_state=25)
@@ -66,8 +80,6 @@ def clasificar_piernas():
     parametros = {'C':[1,10,100,1000],
                  'gamma': [0.001, 0.005, 0.01, 0.1]}
     
-    parametros_linear = {'C':[1,10]}
-
     svc_rbf = SVC(kernel='rbf')
     svc_linear = SVC(kernel='linear')
     svc_poly_2 = SVC(kernel='poly', degree=2)
@@ -75,11 +87,13 @@ def clasificar_piernas():
     svc_poly_4 = SVC(kernel='poly', degree=4)
     
     print("Entrenando modelos, esto puede tardar un poco")
-    scores_rbf, grid_rbf = probarSVM(datos_x, datos_y, x_train, y_train, x_test, y_test, svc_rbf, parametros)
-    scores_linear, grid_linear = probarSVM(datos_x, datos_y, x_train, y_train, x_test, y_test, svc_linear, parametros_linear)
-    scores_poly_2, grid_poly_2 = probarSVM(datos_x, datos_y, x_train, y_train, x_test, y_test, svc_poly_2, parametros)
-    scores_poly_3, grid_poly_3 = probarSVM(datos_x, datos_y, x_train, y_train, x_test, y_test, svc_poly_3, parametros)
-    scores_poly_4, grid_poly_4 = probarSVM(datos_x, datos_y, x_train, y_train, x_test, y_test, svc_poly_4, parametros)
+    print("Entrenando rbf")
+    scores_rbf, grid_rbf = entrenar_grid(datos_x, datos_y, x_train, y_train, x_test, y_test, svc_rbf, parametros)
+    scores_linear, svc_linear = entrenar_modelo(datos_x, datos_y, x_train, y_train, x_test, y_test, svc_linear)
+    scores_poly_2, svc_poly_2 = entrenar_modelo(datos_x, datos_y, x_train, y_train, x_test, y_test, svc_poly_2)
+    scores_poly_3, svc_poly_3 = entrenar_modelo(datos_x, datos_y, x_train, y_train, x_test, y_test, svc_poly_3)
+    scores_poly_4, svc_poly_4 = entrenar_modelo(datos_x, datos_y, x_train, y_train, x_test, y_test, svc_poly_4)
+
 
     mejor_score = max(scores_rbf.mean(), scores_linear.mean(), scores_poly_2.mean(), scores_poly_3.mean(), scores_poly_4.mean())
     
@@ -88,25 +102,24 @@ def clasificar_piernas():
         print(grid_rbf.best_params_)
         grid_search = grid_rbf
     elif comparar_reales(mejor_score, scores_linear.mean()):
-        print("El mejor estimador es SVC con el kernel linear con los siguientes parámetros: ")
-        print(grid_linear.best_params_)
-        grid_search = grid_linear
+        print("El mejor estimador es SVC con el kernel linear: ")
+        grid_search = svc_linear
     elif comparar_reales(mejor_score, scores_poly_2.mean()):
-        print("El mejor estimador es SVC con el kernel polinomico de grado 2 con los siguientes parámetros: ")
-        print(grid_poly_2.best_params_)
-        grid_search = grid_poly_2
+        print("El mejor estimador es SVC con el kernel polinomico de grado 2 ")
+        grid_search = svc_poly_2
     elif comparar_reales(mejor_score, scores_poly_3.mean()):
-        print("El mejor estimador es SVC con el kernel polinomico de grado 3 con los siguientes parámetros: ")
-        print(grid_poly_3.best_params_)
-        grid_search = grid_poly_3
+        print("El mejor estimador es SVC con el kernel polinomico de grado 3 ")
+        grid_search = svc_poly_3
     elif comparar_reales(mejor_score, scores_poly_4.mean()):
-        print("El mejor estimador es SVC con el kernel polinomico de grado 4 con los siguientes parámetros: ")
+        print("El mejor estimador es SVC con el kernel polinomico de grado 4 ")
         print(grid_poly_4.best_params_)
-        grid_search = grid_poly_4
+        grid_search = svc_poly_4
         
         
-        
+    print("Y con un accuracy 5-cross validation: %0.4f (+/- %0.4f)" % (mejor_score.mean(), mejor_score.std() * 2))
+
     # guardamos el clasificador
     with open("mejor_clasificador.pkl", "wb") as archivo:
         pickle.dump(grid_search, archivo)
+        
         
